bef4bf5bea47872f1f1b4a671f917f9f106a54ab
deepthi <deepthirera@gmail.com>
2013-12-14 10:19:40 +0530

    add clone

--- spec/javascripts/form_cloner_spec.js
+++ spec/javascripts/form_cloner_spec.js
@@ -1,9 +1,22 @@
 describe("Form cloner", function() {
-	it("click should show the content", function(){
+  it("click add_clone should clone from to_clone and add to clone-append-section", function(){
     loadFixtures("sample_form.html");
     FormCloner.init();
-    $(".to_show").hide();
-    $("#add_new").click();
-	  expect($(".to_show")).toBeVisible();
-	});
+    $('#add_details').click();
+    $('#add_details').click();
+    $('#add_address').click();
+
+    expect($('[data-clone-append-section="add_details"]')).toHaveHtml('<label>Detail</label><input type="text"/><label>Detail</label><input type="text"/>');
+    expect($('[data-clone-append-section="add_address"]')).toHaveHtml('<input type="text"><input type="text"><input type="text"><input type="text">');
+  });
+
+  it("click add_clone should trigger cloneComplete on appender", function(){
+    loadFixtures("sample_form.html");
+    FormCloner.init();
+    $('[data-clone-append-section="add_details"]').on('cloneComplete', function(evt, clonedContent){
+      clonedContent.append('<span>Done</span>');
+    });
+    $('#add_details').click();
+    expect($('[data-clone-append-section="add_details"]')).toContainHtml('<span>Done</span>');
+  });
 })
--- src/form_cloner.js
+++ src/form_cloner.js
@@ -1,7 +1,26 @@
 var FormCloner = {
   init: function(){
-    $("#add_new").click(function(){
-      $(".to_show").show()
+    $.each($('.add_clone'), function(index, element){
+      new formCloner($(element));
     });
   }
+}
+
+function formCloner(addCloneButton){
+  var cloneIdentifier = addCloneButton.attr('id'),
+      cloneAppendSection = _correspondingElement('data-clone-append-section'),
+      cloneTemplate = _correspondingElement('data-clone-template');
+
+  function addClone(){
+    var cloneContent = cloneTemplate.find('.to_clone').children().clone(true);
+    cloneAppendSection.append(cloneContent);
+    cloneAppendSection.trigger('cloneComplete', [cloneContent]);
+  }
+
+  // $('[data-clonable-content="button_id"]')
+  function _correspondingElement(dataAttr){
+    return $('['+ dataAttr + '="'+ cloneIdentifier + '"]');
+  }
+
+  addCloneButton.click(addClone);
 }
